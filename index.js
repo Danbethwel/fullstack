2550e9f12d602dbb944bcf24c790308bd47aee11
Danbethwel <93147941+Danbethwel@users.noreply.github.com>
2023-06-25 16:22:11 +0300

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-let fname="Ryan";
-let lname="Kasike";
-let age = prompt("Guess Ryan`s age..");
-
-
-//old way
-//let result = fname +  " "  + lname +  age  + "years old";
-//alert(result);
-
-//using template strings
-let result=`${fname} ${lname} is ${age} years old`;
-alert (result);
+function greeting(message){
+    return alert(`${message} everyone`);
+}
+greeting('Good morning');
