478cc4548205c8985e46fd639b888bca048f2811
Danbethwel <93147941+Danbethwel@users.noreply.github.com>
2023-06-25 16:42:08 +0300

    new commit

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-function greeting(message){
-    return alert(`${message} everyone`);
-}
-greeting('Good morning');
+//function greeting(message){
+//    return alert(`${message} everyone`);
+//}
+
+// let greeting = (message)=>alert(`${message} everyone`);
+
+// greeting('Good Morning');
+
+let createBlog =(title,body) => {
+    if (!title){
+        throw new Error('A title is required');
+    }
+
+    if (!body){
+        throw new Error('Body can`t be empty')
+    }
+
+    return alert(`${title} - ${body}`);
+};
+
+createBlog("Blog title", 'Blog Body');
